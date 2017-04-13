7671b56d5abf96591494e5efdd50253b3880b41a
achavezc <anderson.chavezc@gmail.com>
2017-04-12 22:48:03 -0500

    changes

--- Config/Config.js
+++ Config/Config.js
@@ -72,13 +72,22 @@ config.prototype.parameters = function(data){ return {
         return n;
       },
       on: "time"
-    }], //aqui se puede configurar valores por defecto en este caso no aplica dont apply
+    },
+	{
+	run: function (value) {
+		if(value == null){
+			return "";
+		}
+      }	
+	,
+	on: "origin"
+	}], //aqui se puede configurar valores por defecto en este caso no aplica dont apply
 	plantillaJsonDestino: 
 	{
 	"event":"$.event",
 	"distinct_id":"$.properties.distinct_id",
 	"time":"$.properties.time",
-	//"origin":"$.properties.origin",
+	"origin":"$.properties.origin",
 	//"origin_referrer":"$.properties.origin_referrer",
 	"initial_referring_domain":"$.properties.$initial_referring_domain",
 	"referrer":"$.properties.$referrer",
