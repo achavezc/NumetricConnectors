2c14405d7c1aca621c69d11efcde345c70f92d77
achavezc <anderson.chavezc@gmail.com>
2017-04-18 19:09:33 -0500

    cleaned IndexShopify

--- IndexShopify.js
+++ IndexShopify.js
@@ -1,12 +1,9 @@
-'use strict'
-const NumetricCon = require("./DataConnectorApi/NumetricDataConnectorApi/NumetricDataConnectorApi")
-const ShopifyCon = require("./DataConnectorLogic/ShopifyDataConnectorLogic")
-const numetricDataConnectorLogic = require("./DataConnectorLogic/NumetricDataConnectorLogic")
-const ShopifyData = require("./DataConnectorApi/ShopifyDataConnectorApi/ShopifyDataDataConnectorApi")
-const config = require("./Config/Config")
-const utils = require("./Helper/Util")
-const ShopifyDataConSync = require("./DataConnectorSync/ShopifyDataConnectorSync")
-const promiseRetry = require('promise-retry')
+'use strict';
+
+const ShopifyData = require("./DataConnectorApi/ShopifyDataConnectorApi/ShopifyDataDataConnectorApi");
+const config = require("./Config/Config");
+const utils = require("./Helper/Util");
+const ShopifyDataConSync = require("./DataConnectorSync/ShopifyDataConnectorSync");
 var conf = new config();
 var datetime = require('node-datetime');
 var cron = require('node-cron');
@@ -43,7 +40,7 @@ var onJobStarted = function()
 	  created_at_min : dateInitial,
 	  timezone :conf.parameters().timezone,
 	  limit: conf.parameters().limitShopify
-	}
+	};
 
 	//'01/06/2017 4:52:48 PM',
 
@@ -59,47 +56,10 @@ var onJobStarted = function()
 			ShopifyDataConSync.syncDataRetry(lastUpdated);
 		}
 	});
-	
-    return;
 };
 
 
 cron.schedule(shopifyJobFrequency, onJobStarted);
-
-/*
-
-var nconf = require('nconf');
-nconf.use('file', { file: './ConfigDate/DateTimeLastSync.json' });
-nconf.load();
-	
-var dateInitial = "";
-
-if(nconf.get('lastUpdateShopify')!= "")
-{
-	dateInitial = nconf.get('lastUpdateShopify');
-}
-else
-{
-	dateInitial = conf.parameters().initialDateTimeShopify;
-}
-	
-
- return NumetricCon.getDataSetNumetric().then(currentListDataset=>
- {	
-	 ShopifyDataConSync.syncDataTransactions(lastUpdated,currentListDataset);
- })
- */
-
-
-/*
-
-return NumetricCon.getDataSetNumetric().then(currentListDataset=>
-{	
-	ShopifyDataConSync.syncDataCustomer(lastUpdated,currentListDataset);
-})
-*/
-
-
 
 
 
